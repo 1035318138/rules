9008f35ef394a481eab11505fbbbbafa1007fb72
Can.Ru <can.ru@lianwei.com.cn>
2024-05-31 10:37:25 +0800

    秒杀

--- src/main/java/rules/controller/RuleController.java
+++ src/main/java/rules/controller/RuleController.java
@@ -1,8 +1,17 @@
 package rules.controller;
 
+import org.apache.commons.logging.LogFactory;
 import org.easyrules.api.RulesEngine;
 import org.easyrules.core.RulesEngineBuilder;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,10 +24,7 @@ import threadLocal.utils.AESUtil;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.*;
 
 /**
  * @author Can.Ru
@@ -26,6 +32,7 @@ import java.util.concurrent.ThreadPoolExecutor;
 @RestController
 @RequestMapping("/rule")
 public class RuleController {
+
 
     @Autowired
     private RuleService ruleService;
@@ -99,5 +106,58 @@ public class RuleController {
 
         }
         return "s";
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return redisTemplate;
+    }
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    @Autowired
+    private RedissonClient redissonClient;
+
+    // 模拟库存数量
+    private static final String STOCK_KEY = "seckill:stock";
+    // Redis 分布式锁的 key
+    private static final String LOCK_KEY = "seckill:lock:";
+
+    @Autowired
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    @RequestMapping("/flashSale")
+    public String flashSale(){
+
+        //假设入参商品为mock1
+        String productCode = "mock1";
+        RLock lock = redissonClient.getLock(LOCK_KEY + productCode);
+        try {
+            boolean isLock = lock.tryLock(100, 10000, TimeUnit.MILLISECONDS);
+            if (isLock) {
+                try {
+                    Long stock = redisTemplate.opsForValue().decrement("rules:flashSale:mock1");
+                    if (stock != null && stock >= 0) {
+                        namedParameterJdbcTemplate.getJdbcTemplate().execute("update test.stock set stock=stock-1 where id =1");
+                        return "扣减成功";
+                    } else {
+                        return "库存不足";
+                    }
+                } finally {
+                    lock.unlock();
+                }
+
+
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        return "未获取锁";
     }
 }
